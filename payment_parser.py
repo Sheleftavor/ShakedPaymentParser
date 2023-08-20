70214d703e47583a1692f31a7cae19e90ea93480
Shelef Tavor <sheleftavor@gmail.com>
2023-08-20 23:21:01 +0300

    Initial commit

---
+++ payment_parser.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+"""
+Purpose: CLI part for the parser
+Author: Shelef Tavor (20.08.23)
+"""
+
+import click
+
+
+def main():
+    """ The main function of the class """
+
+
+if __name__ == "__main__":
+    main()
